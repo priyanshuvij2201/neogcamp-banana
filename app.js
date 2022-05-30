08e18212589d4664049c2ebc8076417bb64783f6
priyanshuvij2201 <PRIYANSHUVIJ456@GMAIL.COM>
2022-05-31 00:52:53 +0530

    taking input and output

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var username = prompt("Give me your username")
-var welcomemessage = "this script works " + username
-alert(welcomemessage)
+var btnTranslate = document.querySelector("#btn-translate");
+
+ 
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+
+ btnTranslate.addEventListener("click",function clickEventHandler(){
+ console.log("clicked!")
+console.log("input",txtInput.value)})
